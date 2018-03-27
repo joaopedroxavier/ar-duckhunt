d185d3361a3636fd6ac360e13b60acd218a7bbe6
Joao Pedro Xavier <joaopedroxavier@dhcp-147-65-6-21.impa.br>
2018-03-27 18:57:02 -0300

    Fixed Audio bug
    - Mute button is working now

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance;
+
+	void Start() {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	public void Toggle() {
+		AudioSource player = GetComponent<AudioSource> ();
+		player.mute = !player.mute;
+	}
+}
