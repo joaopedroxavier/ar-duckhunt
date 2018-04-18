106850d941d3e1c97f1de53508a055c0187eff9b
Joao Pedro Xavier <joaopedroxavier@dhcp-147-65-6-21.impa.br>
2018-04-18 17:34:09 -0300

    Added Duck Icon
    - HUD Component

---
+++ Assets/Scripts/DuckIconController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DuckIconController : MonoBehaviour {
+
+	public Sprite normal;
+	public Sprite highlighted;
+	public int presses = 0;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Image>().sprite = highlighted;
+	}
+
+	void turnOff() {
+		Image image = GetComponent<Image> ();
+		image.sprite = normal;
+	}
+
+	void turnOn() {
+		Image image = GetComponent<Image> ();
+		image.sprite = highlighted;
+	}
+
+	void hide() {
+		Image image = GetComponent<Image> ();
+		image.enabled = false;
+	}
+
+	void show() {
+		Image image = GetComponent<Image> ();
+		image.enabled = true;
+	}
+}
